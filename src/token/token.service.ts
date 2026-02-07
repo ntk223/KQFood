8b464c139a9e2dc5053a26c9383bcf9b6876d7bd
ntk223 <kienlx2005@gmail.con>
2026-02-07 17:02:06 +0700

    feat: implement user registration and role management with profile creation

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-
+import { RoleType } from '@/constants/role';
 export type AccessTokenPayload = {
   sub: number;
-  email: string;
+  roles: RoleType[];
 };
 
 export type RefreshTokenPayload = {
